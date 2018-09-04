b4c0ce17f5b74781694e1c817ea616e460ccc735
Azraar Azward <azraar@keeneye.solutions>
2018-09-04 19:17:49 +0530

    Initial Commit

---
+++ Client/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8000/',
+  clientSecret: '12lu3rfrLuQEBV1ANfqVdA9xaHdR0fduNCG1x7UR',
+  clientId: 2,
+  grantType: 'password',
+};
